efba93b8644f374c3db10bb12b856bd53fa44e91
chrismuldoon <8739779+chrismuldoon@users.noreply.github.com>
2021-03-30 03:32:55 -0400

    project v1

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+	var years = parseInt(document.getElementById("years").value);
+	
+	var date = new Date();
+	var future_year = years + date.getFullYear();
+	var interest = principal * years * rate / 100
+	
+	//clear
+	document.getElementById("result_span").innerHTML = '';
+	
+	//validation
+	if (principal <= 0) {
+		alert("Please enter a positive amount for Amount");
+		document.getElementById("principal").focus();
+		return;
+	}
+	
+	//display
+	document.getElementById("result_span").innerHTML = '<br><BR>If you deposit $'+principal+
+														', <br>at an interest rate of '+rate+
+														'%, <br>you will recive an amount of $'+interest+
+														', <br>in the year '+future_year+'.';
 }
-        
+		
+function disp_rate_range() {
+	var val = document.getElementById("rate").value;
+	document.getElementById("rate_span").innerHTML = val + "%";
+	
+	
+}
